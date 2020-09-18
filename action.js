6f179bf13119355afdad106f4ce2a92f2fcf5704
Steven Blatnick <sblatnick@proofpoint.com>
2020-09-18 14:50:46 -0600

    catch links added later

--- action.js
+++ action.js
@@ -2,15 +2,38 @@
 
 var settings = {};
 
+function target_links() {
+  if(frames[0].document) {
+    $("a", frames[0].document).attr({target: "right-frame"});
+  }
+  setTimeout(target_links, 2000);
+
+  /*
+  $("a", frames[0].document).attr({target: "right-frame"})
+    .on('ajaxComplete',function() {
+      $("a", frames[0].document).attr({target: "right-frame"});
+    });
+  */
+  /*
+  MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+  var observer = new MutationObserver(function(mutations, observer) {
+    console.log(mutations, observer);
+    $("a", $(mutations.target)).attr({target: "right-frame"});
+  });
+  observer.observe(frames[0].document, {
+    subtree: true,
+    attributes: true
+  });
+  */
+}
+
 function main()
 {
   var left = settings.left.width;
   var right = 100 - left;
   var cols = left +'%,' + right + '%';
   document.write('<frameset cols="' + cols + '"><frame id="left-frame" name="left-frame" src="' + document.URL + '" /><frame name="right-frame" src="" /></frameset>');
-  $("#left-frame").on("load", function() {
-    $("a", frames[0].document).attr({target: "right-frame"});
-  });
+  $("#left-frame").on("load", target_links);
 }
 
 chrome.storage.sync.get('settings', function(data)
