25b53082c3c8287979984fc10b21a3ec851a74df
Steven Blatnick <sblatnick@proofpoint.com>
2020-09-28 17:18:58 -0600

    Don't edit headers when plugin not activated (still a problem with frames from initial page)

--- background.js
+++ background.js
@@ -5,6 +5,7 @@ var settings = {
     width: 50
   }
 };
+var activated = -1;
 
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({"settings": settings}, function() {
@@ -13,6 +14,8 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
+  activated = tab.id;
+  //console.log("tab:", tab);
   chrome.tabs.executeScript(tab.id, {file: "jquery-3.5.1.min.js"}, function() {
     chrome.tabs.executeScript(tab.id, {file: "action.js"});
   });
@@ -21,20 +24,21 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 chrome.webRequest.onHeadersReceived.addListener(
   info => {
     const headers = info.responseHeaders;
-    //console.log(headers);
-    for (let i=headers.length-1; i>=0; --i) {
-      let header = headers[i].name.toLowerCase();
-      //console.log("Header: {" + header + ": " + headers[i].value + "}");
-      if(header === "x-frame-options" || header === "frame-options") {
-        console.log("Removed: " + header);
-        headers.splice(i, 1);
-      }
-      if(header === "content-security-policy") {
-        let old = headers[i].value;
-        let edited = "frame-ancestors https:" //FIXME: https hardcoded
-        console.log("Edited: " + old + " -> " + edited);
-        headers[i].value = edited;
-        //headers[i].value = headers[i].value.replace("frame-src", "frame-src https:");
+    //console.log("info:", info);
+    if(activated == info.tabId) {
+      for (let i=headers.length-1; i>=0; --i) {
+        let header = headers[i].name.toLowerCase();
+        //console.log("Header: {" + header + ": " + headers[i].value + "}");
+        if(header === "x-frame-options" || header === "frame-options") {
+          console.log("Removed: " + header);
+          headers.splice(i, 1);
+        }
+        if(header === "content-security-policy") {
+          let old = headers[i].value;
+          let edited = "frame-ancestors https:" //FIXME: https hardcoded
+          console.log("Edited: " + old + " -> " + edited);
+          headers[i].value = edited;
+        }
       }
     }
     return {responseHeaders: headers};
